6abb98e1cdcf372edd57f9dff347c7e70b705b7a
Koflerj93 <koflerj93@gmail.com>
2020-11-17 22:21:21 -0500

    commit tree 5940a60bb09ca97f56c73477b27efd2b3157ddd1

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+
+var customerName = "bob"
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+    console.log(bestCustomer = "not bob");
+    return bestCustomer = "not bob"
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = "maybe bob"
+} 
+
+const leastFavoriteCustomer = "Henry"
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "not Henry"
+}
